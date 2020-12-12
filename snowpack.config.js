524fe97bffa633f462df87b2bbc2681554464744
Juno <juno.burger@pm.me>
2020-12-12 18:20:11 +0100

    first commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [],
   install: [
     /* ... */
   ],
